601a60c7c209fbd4ee7774afedbd82c5eefeffaf
unknown <vadimbelinovich@gmail.com>
2021-09-02 19:31:42 +0300

    added brands

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,74 +4,44 @@
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import Flickity from 'flickity';
-
 import showYear from './modules/year';
+
 // const targetBurger = document.querySelector('.target-burger');
 // const nav = document.querySelector('.nav.nav-mobile');
 // const main = document.querySelector('main.main');
 // const footer = document.querySelector('footer');
 // const body = document.querySelector('body');
 const year = document.querySelector('.year');
+const ARROW = 'M0 50.3489C0 50.4822 0.0596932 50.5823 0.179077 50.6656L13.0924 61.5683C13.1919 61.6516 13.3113 61.7016 13.4705 61.7016C13.7888 61.7016 14.0077 61.5016 14.0077 61.2682C14.0077 61.1348 13.9679 61.0015 13.8684 60.9181L5.75033 54.1165L1.65147 50.7323L5.65084 50.7823H99.4827C99.7612 50.7823 100 50.5989 100 50.3489C100 50.0988 99.7612 49.8988 99.4827 49.8988H5.65084L1.65147 49.9488L5.75033 46.5813L13.8684 39.7629C13.9878 39.6796 14.0077 39.5629 14.0077 39.4295C14.0077 39.1961 13.7888 38.9961 13.4705 38.9961C13.3113 38.9961 13.1521 39.0294 12.8736 39.2795L0.179077 50.0154C0.0596932 50.1155 0 50.2155 0 50.3489Z';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    // показать актуальный год в футере
-    showYear(year);
+  // показать актуальный год в футере
+  showYear(year);
 
+  // slider на главной для раздела Обучение
+  try {
+    const flky = new Flickity('.courses', {
+      pageDots: false,
+      freeScroll: true,
+      watchCSS: true,
+      arrowShape: ARROW,
+      cellAlign: 'left'
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
-    // slider на главной для раздела Обучение
-    try {
-        const flky = new Flickity('.courses', {
-            pageDots: false,
-            freeScroll: true,
-            watchCSS: true,
-            arrowShape: 'M 0,50 L 60,00 L 50,30 L 80,30 L 80,70 L 50,70 L 60,100 Z',
-            cellAlign: 'left'
-        });
-    } catch (e) {
-        console.log(e);
-    }
-
-    //   // home first slider
-    //   try {
-    //     if (window.innerWidth < 1600) {
-    //       sliderWithSetings({
-    //         slide: '.offer__slide',
-    //         slider: "[data-slider]",
-    //         prevArrow: ".offer__slider-prev",
-    //         nextArrow: ".offer__slider-next",
-    //         totalCounter: '#total',
-    //         currentCounter: '#current',
-    //         arrowsPar: true,
-    //         dotsPar: false,
-    //         speedPar: 700,
-    //         firstSlidePar: 0,
-    //         variableWidthPar: false,
-    //         centerModePar: false,
-    //         slidesToShowPar: 1,
-    //         slidesToScrollPar: 1
-    //       });
-    //     } else {
-    //       sliderWithSetings({
-    //         slide: '.offer__slide',
-    //         slider: "[data-slider]",
-    //         prevArrow: ".offer__slider-prev",
-    //         nextArrow: ".offer__slider-next",
-    //         totalCounter: '#total',
-    //         currentCounter: '#current',
-    //         arrowsPar: true,
-    //         dotsPar: false,
-    //         speedPar: 1500,
-    //         firstSlidePar: 0,
-    //         variableWidthPar: true,
-    //         centerModePar: false,
-    //         slidesToShowPar: 1,
-    //         slidesToScrollPar: 1
-    //       });
-    //     }
-
-    //   } catch (e) {
-    //     console.log(e);
-    //   }
+  // slider на главной для раздела Бренды
+  try {
+    const flky = new Flickity('.brands-carousel', {
+      pageDots: false,
+      freeScroll: true,
+      arrowShape: ARROW,
+      cellAlign: 'left'
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
 });
