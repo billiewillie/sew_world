d7c13db5dc7d4b457b4fddd85b1e10ecd6475e76
unknown <vadimbelinovich@gmail.com>
2021-09-09 19:21:37 +0300

    added gsap for gallery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,10 @@
 // imports
 import toggleBurgers from './modules/toggleBurger';
 import getGallery from './modules/getGallery';
+import gsap from 'gsap';
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 // varibles
 const burgers = document.querySelectorAll('.burger');
@@ -12,5 +16,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
   toggleBurgers(burgers);
   getGallery(galleryTabs);
+
+  gsap.set(".gallery-item__middle", {y: 80});
+  gsap.to('.gallery-item__middle', {
+    scrollTrigger: {
+      trigger: '.gallery',
+      scrub: true,
+      toggleActions: "play none none none"
+    },
+    y: -80,
+  })
 
 });
